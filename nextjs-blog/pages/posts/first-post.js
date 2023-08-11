bd45c36c32faac9213a671f4470c38aced627fe1
sicp618 <cc191954@gmail.com>
2023-08-11 15:16:34 +0800

    add css

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -2,10 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
                 <script src="https://connect.facebook.net/en_US/sdk.js" />
@@ -17,12 +18,12 @@ export default function FirstPost() {
             />
             <h1>First Post</h1>
             <h2>
-                <Link href="/">Back to home</Link>
+                <Link href="/">Back to home by /Link</Link>
             </h2>
             <h2>
-                <a href="/">Back to home</a>
+                <a href="/">Back to home by /a</a>
             </h2>
             <Image src="/images/profile.jpg" height={144} width={144} alt="Your Name" />
-        </>
+        </Layout>
     );
 }
